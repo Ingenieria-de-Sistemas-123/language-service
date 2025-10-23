04fbf9f010148c364f52d42c635e2fe2a78bb5e3
Abrojito <abrilbrieva@gmail.com>
2025-10-23 10:10:24 -0300

    spotlessApply

--- src/main/java/com/snippetsearcher/language/LanguageServiceApplication.java
+++ src/main/java/com/snippetsearcher/language/LanguageServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LanguageServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LanguageServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LanguageServiceApplication.class, args);
+  }
 }
--- src/main/java/com/snippetsearcher/language/service/ParserService.java
+++ src/main/java/com/snippetsearcher/language/service/ParserService.java
@@ -3,5 +3,4 @@ package com.snippetsearcher.language.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ParserService {
-}
+public class ParserService {}
